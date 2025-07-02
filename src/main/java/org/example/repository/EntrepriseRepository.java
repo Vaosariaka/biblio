03b323f6e85ef7adab32d0ecbc94788f4ba62abf
Vaosariaka Arisolo Elina <Andriamanantsoa.vaosariaka@gmail.com>
2025-07-02 22:19:58 +0300

    delete

--- src/main/java/org/example/repository/EntrepriseRepository.java
+++
@@ -1,9 +0,0 @@
-// package org.example.repository;
-
-// import org.example.entity.Entreprise;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import java.util.Optional;
-
-// public interface EntrepriseRepository extends JpaRepository<Entreprise, Integer> {
-//     Optional<Entreprise> findByNom(String nom);
-// }
--- src/main/java/org/example/repository/PosteRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.entity.Poste;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PosteRepository extends JpaRepository<Poste, Integer> {
-}
